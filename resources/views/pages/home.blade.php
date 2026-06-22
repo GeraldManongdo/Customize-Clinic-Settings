334587b050128018d892c20ab3fe4116f2a7489e
gerald <geraldmanongdo912@gmail.com>
2026-06-23 01:12:21 +0800

    first commit

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Welcome to the Clinic Management System</h1>
+    <p>This is the home page. Use the navigation menu to access different sections of the application.</p>
+@endsection
